56e1ecc41a126e3e771589a8e51b5a811c2382b0
chalermporn17 <chalermporn@eruditetech.net>
2025-02-03 15:46:26 +0700

    fix: Wrong workflow variable in bash script

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,8 +7,8 @@ fi
 echo '{"name":"GitHub Actions","type":"github","reportName":"'"$REPORT_NAME"'",' > executor.json
 echo "\"url\":\"${REPORT_URL}\"," >> executor.json # ???
 echo "\"reportUrl\":\"${REPORT_URL}\"," >> executor.json
-echo "\"buildUrl\":\"${INPUT_GITHUB_SERVER_URL}/${{ github.repository }}/actions/runs/${{ github.run_id }}\"," >> executor.json
-echo "\"buildName\":\"GitHub Actions Run #${{ github.run_id }}\",\"buildOrder\":\"${{ github.run_number }}\"}" >> executor.json
+echo "\"buildUrl\":\"${GITHUB_SERVER_URL}/${GITHUB_REPOSITORY}/actions/runs/${WORKFLOW_RUN_ID}\"," >> executor.json
+echo "\"buildName\":\"GitHub Actions Run #${WORKFLOW_RUN_ID}\",\"buildOrder\":\"${WORKFLOW_RUN_NUMBER}\"}" >> executor.json
 
 cat executor.json
 
